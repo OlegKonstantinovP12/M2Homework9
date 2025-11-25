7e6a6382e58e77b04d55f7aa6bd026d812ab6070
Oleg Konstantinov <oleg.boss@gmail.com>
2025-11-25 21:54:21 +0100

    Initial Commit

---
+++ M2Homework9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2Homework9
+//
+//  Created by Oleg Konstantinov on 25.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
